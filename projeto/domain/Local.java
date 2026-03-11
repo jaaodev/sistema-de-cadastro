d7e30bae56d9586e6ca6a6402bf28a317ff7ca23
João Rocha <rochajoaao@gmail.com>
2026-03-11 17:05:46 -0300

    Primeiro commit do sistema de cadastros

---
+++ projeto/domain/Local.java
@@ -0,0 +1,33 @@
+package projeto.domain;
+
+import java.util.Scanner;
+
+public class Local {
+    private String endereco;
+
+    public Local() {
+    }
+
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Local cadastrarEndereco(Scanner input) {
+        this.endereco = lerTexto(input, "Endereço (Local): ");
+        System.out.println("Seminário cadastrado com sucesso!");
+        return this;
+    }
+
+    private static String lerTexto(Scanner input, String prompt) {
+        System.out.print(prompt);
+        return input.nextLine().trim();
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
